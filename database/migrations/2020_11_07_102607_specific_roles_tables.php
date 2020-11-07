d146cf1dec29519ceb2e4863c8ddb1e2ffe93ece
domgud <47145742+domgud@users.noreply.github.com>
2020-11-07 13:25:24 +0200

    Specific role tables
    Added migrations for specific role tables

---
+++ database/migrations/2020_11_07_102607_specific_roles_tables.php
@@ -0,0 +1,80 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class SpecificRolesTables extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('state', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table-> string('name');
+
+        });
+        Schema::create('admin', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table -> dateTime('work_start');
+            $table -> dateTime('end_work');
+            $table->bigInteger('state_id')->unsigned();
+            $table->bigInteger('user_id')->unsigned();
+        });
+        Schema::table('admin', function($table) {
+            $table->foreign('state_id')->references('id')->on('state')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+        Schema::create('trainer', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table -> dateTime('work_start');
+            $table -> dateTime('end_work');
+            $table->bigInteger('state_id')->unsigned();
+            $table->bigInteger('user_id')->unsigned();
+            $table->integer('experience');
+            $table->boolean('is_private');
+        });
+        Schema::table('trainer', function($table) {
+            $table->foreign('state_id')->references('id')->on('state')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+        Schema::create('client', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->date('created_date');
+            $table->bigInteger('user_id')->unsigned();
+        });
+        Schema::table('client', function($table) {
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('trainer', function($table) {
+            $table-> dropForeign('trainer_state_id_foreign');
+            $table-> dropForeign('trainer_user_id_foreign');
+
+        });
+        Schema::table('admin', function($table) {
+            $table-> dropForeign('admin_state_id_foreign');
+            $table-> dropForeign('admin_user_id_foreign');
+
+        });
+        Schema::table('client', function($table) {
+            $table-> dropForeign('client_user_id_foreign');
+
+        });
+        Schema::dropIfExists('trainer');
+        Schema::dropIfExists('admin');
+        Schema::dropIfExists('client');
+    }
+}
