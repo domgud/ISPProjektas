991ecf87a2244599ecb6791030489473566b857c
lukasdevenas <59114037+lukasdevenas@users.noreply.github.com>
2020-11-08 15:57:55 +0200

    Updated register form
    Updated register form where I added time fields where the user can enter his time for the training.

--- resources/views/visit/register.blade.php
+++ resources/views/visit/register.blade.php
@@ -7,6 +7,18 @@
                 <div class="card-header">{{ __('Registracija į treniruotę') }}</div>
                 <div><b>Registracijos forma</b></div>
                 <br>
+                <div>Įveskite norimus laikus:</div>
+                <br>
+                <form>
+                    <label for="nuo">Nuo:</label>
+                    <input type="text" id="nuo" name="nuo">
+                    <label for="iki">Iki:</label>
+                    <input type="text" id="iki" name="iki">
+                    <input type="submit" value="Ieškoti treniruotės" class="btn btn-primary">
+                </form>
+
+                <br>
+
                 @can('create-visit')
                     <a href="{{route('visit.index')}}"> <button type="button" class="btn btn-warning float-left">Registruotis</button> </a>
                     <br>
